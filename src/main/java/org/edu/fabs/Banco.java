202b3f6cf2c56e43b445544a446526854abea5e2
bianavic <bianavic@gmail.com>
2023-10-20 19:47:41 -0300

    criacao classes, interfaces e metodos para desafio banco digital

---
+++ src/main/java/org/edu/fabs/Banco.java
@@ -0,0 +1,10 @@
+package org.edu.fabs;
+
+import java.util.List;
+
+public class Banco {
+
+    private String nome;
+    private List<Conta> contas;
+
+}
---
+++ src/main/java/org/edu/fabs/Transacao.java
@@ -0,0 +1,10 @@
+package org.edu.fabs;
+
+public interface Transacao {
+
+    void sacar(double valor);
+    void depositar(double valor);
+    void transferir(double valor, Conta contaDestino);
+    void imprimirExtrato();
+
+}
